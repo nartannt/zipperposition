1ef70847b2c05f6b26a987313c7e1a518d1b09c9
nartan <bozec.tanguy@gmail.com>
2023-10-12 14:27:38 +0200

    some work on auxilliary functions

--- src/core/monomorphisation.ml
+++ src/core/monomorphisation.ml
@@ -2,10 +2,17 @@
 module T = Term
 module Lit = Literal
 module Ty = Type
+module IT = InnerTerm 
+
+module TermSet = IT.Set
 
 (* TODO make a neat little module with an ergonomic interface *)
 
 (*axilliary functions, here so I remember the names*)
+
+(* given a set of terms, returns the set of function symbols such that all their type
+ * variables are instantiated at least once in the problem*)
+let instantiated_fun_sym term_set = []
 
 (*instantiates type variable in term with given type*)
 (*look into apply1 *)
@@ -19,10 +26,10 @@ let type_vars term = T.Seq.ty_vars
 let is_monomorphic_type ty = not (Ty.Seq.sub ty |> Iter.exists Ty.is_tType)
 
 (*is term monomorhic*)
-let is_monomorphic_term term = T.monomorphic
+let is_monomorphic_term term = T.monomorphic term
 
 (*types of term*)
-let types_of_term = T.ty_vars
+let types_of_term term = T.ty_vars term
 
 (*given a set of terms, returns all the types that appear in those terms*)
 let types_of_terms terms = []
@@ -31,3 +38,5 @@ let types_of_terms terms = []
 (* given sets of polymorphic and monomorphic terms,
  * computes the new terms generated in a single monomorphisation step*)
 let new_terms mono_terms poly_terms = []
+
+
