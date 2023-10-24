2a2cd43530912adc6c393ad5a90727ca4540bc1d
nartan <bozec.tanguy@gmail.com>
2023-10-24 16:32:28 +0200

    continued debugging the monomorphisation implementation, making progress

--- src/core/monomorphisation.ml
+++ src/core/monomorphisation.ml
@@ -14,15 +14,30 @@ module TypeSet = Ty.Set
 
 
 let match_type ty mono_type =
+    let subst = Unif.Ty.matching_same_scope ~scope:0 ~pattern:mono_type ty in
+    (*InnerTerm.show_type_arguments := true;
+    (Printf.printf "\n old ty:      %s " (Ty.to_string ty));
+    (Printf.printf "\n old mono ty: %s " (Ty.to_string mono_type));
+    let new_ty = Subst.Ty.apply Subst.Renaming.none subst (ty, 0) in
+    (Printf.printf "\n new ty:      %s " (Ty.to_string new_ty));
+    InnerTerm.show_type_arguments := false;*)
     (*(Printf.printf "\n mono type: %s" (Ty.to_string mono_type ));
     (*(assert (Iter.is_empty (Ty.Seq.vars mono_type)));*)
     (*(assert (Iter.exists Ty.is_tType (Ty.Seq.sub mono_type) ));*)
     (Printf.printf "\n non-mono type: %s" (Ty.to_string ty ));*)
-    let subst = Unif.Ty.matching ~pattern:(mono_type, 0) (ty, 1) in
     subst
 
 
-let typed_sym term = T.Seq.typed_symbols term
+(* TODO currrent implementation is incorrect because the symbols are taken from the terms as constants
+ * therefore, the associated types are that of the constant and are never instantiated *)
+let typed_sym term = 
+    (*get all applications*)
+    let all_apps = Iter.filter T.is_app (T.Seq.subterms term) in
+    let res = Iter.sort_uniq (T.Seq.typed_symbols term) in
+    (*InnerTerm.show_type_arguments := true;
+    (Iter.iter (fun (s, t) -> (Printf.printf "\n symbol %s, type: %s " (ID.name s) (Ty.to_string t))) res);
+    InnerTerm.show_type_arguments := false;*)
+    res
 
 let all_typed_sym term_set = 
     let all_sym = TermSet.fold (fun term iter -> Iter.union (typed_sym term) iter) term_set Iter.empty in
@@ -56,40 +71,45 @@ let candidate_mono_term term_syms mono_term =
 
 let new_terms_single mono_term_set term =
     let derive_new_term mono_term term =
-        (*let subst_iter = derive_subst mono_term term in
+        let subst_iter = derive_subst mono_term term in
         let apply_subst term subst = Term.of_term_unsafe (Subst.apply Subst.Renaming.none subst ((term: Term.t :> InnerTerm.t), 0)) in
         let new_terms_iter = Iter.filter_map (fun subst -> try Some(apply_subst term subst) with _ -> None) subst_iter in
-        Iter.to_set (module TermSet) new_terms_iter*)
-        TermSet.empty
+        Iter.to_set (module TermSet) new_terms_iter
     in
     let term_syms = Iter.map fst (typed_sym term) in
     let candidate_mono_terms = TermSet.filter (candidate_mono_term term_syms) mono_term_set in
     let res = TermSet.fold (fun mono_term term_set -> TermSet.union (derive_new_term mono_term term) term_set) TermSet.empty candidate_mono_terms in
-    (*(Printf.printf "\n %i new terms from %i mono terms\n" (TermSet.cardinal res) (TermSet.cardinal mono_term_set));*)
+    (*(Printf.printf "\n %i new terms from %i mono terms \n" (TermSet.cardinal res) (TermSet.cardinal mono_term_set));*)
+    (*InnerTerm.show_type_arguments := true;
+    (Printf.printf "\n old term: %s " (Term.to_string term));
+    (TermSet.iter (fun t -> (Printf.printf "\n new term: %s " (Term.to_string t))) res);
+    InnerTerm.show_type_arguments := false;*)
     res
     
+
+let new_substs_single mono_term_set term =
+    Iter.flat_map (fun mono_term -> derive_subst mono_term term) (Iter.of_set (module TermSet) mono_term_set)
+
+let new_substs mono_term_set term_set =
+    Iter.flat_map (new_substs_single mono_term_set) term_set
+
+let apply_subst term subst =
+    Term.of_term_unsafe (Subst.apply Subst.Renaming.none subst ((term: Term.t :> InnerTerm.t), 0))
 
 let new_terms mono_terms terms =
-    TermSet.fold (fun term term_set -> TermSet.union term_set (new_terms_single mono_terms term)) TermSet.empty terms
+    (*(Printf.printf "\n we have %i mono terms\n" (TermSet.cardinal mono_terms));
+    (Printf.printf "\n we have %i non-mono terms\n" (TermSet.cardinal terms));
+    (Printf.printf "\nWEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\n");*)
+    let res = TermSet.fold (fun term term_set -> TermSet.union term_set (new_terms_single mono_terms term)) terms TermSet.empty in
+    (*InnerTerm.show_type_arguments := true;
+    (TermSet.iter (fun t -> (Printf.printf "\n new term: %s " (Term.to_string t))) res);
+    InnerTerm.show_type_arguments := false;*)
+    res
 
 let is_monomorphic term = 
-    (*let type_vars = T.ty_vars term in
-    let typed_symbols = T.typed_symbols term in*)
-
-    (*let res = 
-        match Ty.arity (T.ty term) with
-            | Arity (a, b) when a == 0 && b == 0 -> true
-            | _ -> false
-    in*)
-    (*let res = Ty.expected_ty_vars (T.ty term) == 0 in*)
     let res = (List.length (Ty.vars (T.ty term))) == 0 in
-
     (*(Printf.printf "\n type: %s is mono? %b" (Ty.TPTP.to_string (T.ty term)) res);*)
     res
-    
-    (*Ty.VarSet.is_empty type_vars*)
-    
-    (*Iter.exists Ty.is_tType (Ty.Seq.sub (T.ty term))*)
 
 let split_terms term_set =
     let mono_terms = TermSet.filter is_monomorphic term_set in
@@ -104,15 +124,15 @@ let mono_step mono_terms non_mono_terms =
 
 let monomorphised_terms term_set count =
     let mono_terms, non_mono_terms = split_terms term_set in
-    (Printf.printf "\nbeginning with %i monomorphic terms\n" (TermSet.cardinal mono_terms) );
-    (Printf.printf "beginning with %i non-monomorphic terms\n" (TermSet.cardinal non_mono_terms) );
+    (*(Printf.printf "\nbeginning with %i monomorphic terms\n" (TermSet.cardinal mono_terms) );
+    (Printf.printf "beginning with %i non-monomorphic terms\n" (TermSet.cardinal non_mono_terms) );*)
     let rec mono_step_limited counter mono_terms non_mono_terms =
         if counter <= 0 then
             mono_terms, non_mono_terms
         else
             let new_mono, new_non_mono = mono_step mono_terms non_mono_terms in
-            (Printf.printf "\n%i total monomorphic terms\n" (TermSet.cardinal new_mono) );
-            (Printf.printf "%i total non-monomorphic terms\n" (TermSet.cardinal new_non_mono) );
+            (*(Printf.printf "\n%i total monomorphic terms\n" (TermSet.cardinal new_mono) );
+            (Printf.printf "%i total non-monomorphic terms\n" (TermSet.cardinal new_non_mono) );*)
             mono_step_limited (counter - 1) new_mono new_non_mono
     in 
     let mono_terms_res, _ = mono_step_limited count mono_terms non_mono_terms in
@@ -132,14 +152,19 @@ let derive_lits left_term_set right_term_set bool =
     
 
 let monomorphise_lit lit mono_term_set =
-    let open Literal in
     match lit with
-        | Equation (left_term, right_term, bool) ->
-            let left_term_set = new_terms_single mono_term_set left_term in
+        | Literal.Equation (left_term, right_term, bool) ->
+            (*let left_term_set = new_terms_single mono_term_set left_term in
             let left_term_set_mono, _ = split_terms left_term_set in
             let right_term_set = new_terms_single mono_term_set right_term in
             let right_term_set_mono, _ = split_terms right_term_set in
-            derive_lits left_term_set_mono right_term_set_mono bool
+            derive_lits left_term_set_mono right_term_set_mono bool*)
+            let left_subst = new_substs_single mono_term_set left_term in
+            let right_subst = new_substs_single mono_term_set right_term in
+            let new_term_pairs = Iter.map (fun s -> (apply_subst left_term s), (apply_subst right_term s)) (Iter.union left_subst right_subst) in
+            let new_mono_term_pairs = Iter.filter (fun (lt, rt) -> is_monomorphic lt && is_monomorphic rt) new_term_pairs in
+            let new_lits = Iter.map (fun (lt, rt) -> Literal.mk_lit lt rt bool) new_mono_term_pairs in
+            Iter.to_list new_lits
         | _ -> [lit]
 
 let monomorphise_clause literals mono_term_set =
