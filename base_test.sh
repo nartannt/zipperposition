6b9eddd423ea91bd46e874613cde9915fc2ba2e1
nartan <bozec.tanguy@gmail.com>
2023-11-07 19:06:27 +0100

    modified small testing script

--- base_test.sh
+++ base_test.sh
@@ -1,4 +1,4 @@
-echo "base mode"
-./zipperposition.exe --mode best --progress -o none --try-e "./eprover-ho" "$1" -t 15
+#echo "base mode"
+#./zipperposition.exe --mode best --progress -o none --try-e "./eprover-ho" "$1" -t 15
 echo "early e"
 ./zipperposition.exe --mode best --progress -o none --try-e "./eprover-ho" --e-call-point 0.0 "$1" -t 15
