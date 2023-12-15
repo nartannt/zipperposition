2018e292c6c007a5c903e06f1b514d0e30f44a59
nartan <bozec.tanguy@gmail.com>
2023-12-15 17:44:36 +0100

    some minor adjustments to utility scripts

--- base_test.sh
+++ base_test.sh
@@ -1,4 +1,8 @@
-#echo "base mode"
-#./zipperposition.exe --mode best --progress -o none --try-e "./eprover-ho" "$1" -t 15
-echo "early e"
-./zipperposition.exe --mode best --progress -o none --try-e "./eprover-ho" --e-call-point 0.0 "$1" -t 15
+
+while read p; do
+    timeout 45\
+        ./portfolio/portfolio.lams.parallel.py "../tptp_benchmarks/poly_problems/TF1/$p" 30 /tmp/ true | grep "status "
+    timeout 45\
+        ./zipperposition.exe --mode best --progress -o none --e-call-point 0.0 --try-e "./eprover-ho" -t 60 "../tptp_benchmarks/poly_problems/TF1/$p" | grep "status "
+    echo "done $p"
+done < "$1"
--- early_tmp.sh
+++ early_tmp.sh
@@ -5,6 +5,9 @@
 # without monomorphisation
 ./zipperposition.exe --mode best -o none --progress -t 60 ../tptp_benchmarks/poly_problems/...
 
+#portfolio
+./portfolio/portfolio.lams.parallel.py "$filename" 30 /tmp/ true
+
 #connection
 lazio.tcs.ifi.lmu.de -l bozect-rhiwp -p 13522
 # scp command
